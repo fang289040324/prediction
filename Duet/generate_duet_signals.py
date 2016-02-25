6182fd84463ac0bddad4f016d376dfa4426299b6
cegrief <cegrief@gmail.com>
2016-02-24 21:08:15 -0600

    Adding initial Duet audio generation files

---
+++ Duet/generate_duet_signals.py
@@ -0,0 +1,139 @@
+import nussl
+import os
+import scipy.io.wavfile as wav
+import numpy as np
+from scipy.ndimage.interpolation import shift
+import scipy.signal
+import matplotlib.pyplot as plt
+
+
+def generate_dry_mixtures():
+    src_dir = 'audio/seed/'
+    dest_dir = 'audio/mix/'
+    seeds = os.listdir(src_dir)
+    for f in range(0, len(seeds)):
+        for h in range(f, len(seeds)):
+            generate_mixture(src_dir+seeds[f], src_dir+seeds[h],
+                             dest_dir+os.path.splitext(seeds[f])[0]+'_'+os.path.splitext(seeds[h])[0]+'.wav')
+
+
+def generate_reverb_mixtures():
+    src_dir = 'audio/mix/'
+    ir_dir = 'audio/IR/'
+    dest_dir = 'audio/outputs/'
+    for f in os.listdir(src_dir):
+        for h in os.listdir(ir_dir):
+            generate_reverb(src_dir + f, ir_dir + h,
+                            dest_dir+os.path.splitext(f)[0] + '_' + os.path.splitext(h)[0] +'.wav')
+
+
+def generate_duet_histograms(src_dir, out_dir):
+    for f in os.listdir(src_dir):
+        sr, data = wav.read(src_dir+ f)
+        if data.dtype == np.dtype("int16"):
+            data = data / float(np.iinfo(data.dtype).max)
+
+        sig = nussl.AudioSignal(audio_data_array=data.T)
+
+        duet = nussl.Duet(sig, 2, sample_rate=sr)
+
+        if not os.path.exists(out_dir):
+            os.mkdir(out_dir)
+        duet.run()
+
+        duet.plot(out_dir + os.path.splitext(f)[0] + '_2d.png')
+        duet.plot(out_dir + os.path.splitext(f)[0] + '_3d.png', True)
+
+        output_name_stem = out_dir + os.path.splitext(f)[0] + '_duet_source'
+        i = 1
+        for s in duet.make_audio_signals():
+            output_file_name = output_name_stem + str(i) + '.wav'
+            s.write_audio_to_file(output_file_name, sample_rate=sr)
+            i += 1
+
+
+def generate_mixture(src1, src2, fname):
+    sr1, data1 = wav.read(src1)
+    if data1.dtype == np.dtype("int16"):
+        data1 = data1 / float(np.iinfo(data1.dtype).max)
+
+    sr2, data2 = wav.read(src2)
+    if data2.dtype == np.dtype("int16"):
+        data2 = data2 / float(np.iinfo(data2.dtype).max)
+
+    if sr1 != sr2:
+        raise ValueError("Both sources muse have same sample rate")
+
+    sample1 = data1[0:10 * sr1]
+    sample2 = data2[0:10 * sr1]
+    left = sample1 + sample2
+    right = attenuate(sample1, .25) + attenuate(sample2, -.25)
+
+    signal = np.vstack((left, right))
+
+    scipy.io.wavfile.write(fname, sr1, signal.T)
+
+
+def generate_reverb(signal, reverb, fname):
+    sr, data = wav.read(signal)
+    if data.dtype == np.dtype("int16"):
+        data = data / float(np.iinfo(data.dtype).max)
+
+
+    sr_ir, data_ir = wav.read(reverb)
+    if data_ir.dtype == np.dtype("int16"):
+        data_ir = data_ir / float(np.iinfo(data_ir.dtype).max)
+
+    if sr_ir != sr:
+        raise ValueError("Impulse Response must have same sample rate as signal")
+
+    wet_mix = add_reverb(data.T, data_ir.T)
+
+    scipy.io.wavfile.write(fname, sr, wet_mix.T)
+
+
+def plot_stereo(signal, fs):
+    t = np.linspace(0, signal[0].size / fs, num=signal[0].size)
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax1.grid(True)
+    ax1.axhline(0, color='black', lw=2)
+    ax1.plot(t,signal[0])
+
+    ax2 = fig.add_subplot(212, sharex=ax1)
+    ax2.grid(True)
+    ax2.axhline(0, color='black', lw=2)
+    ax2.plot(t,signal[1])
+
+    plt.show()
+
+
+def delay(data, delay):
+    return shift(data, delay)
+
+
+def attenuate(data, attenuation):
+    return data * (1-attenuation)
+
+
+def add_reverb(signal, impulse_response):
+    if len(impulse_response.shape) > 1 :
+        outl = scipy.signal.fftconvolve(signal[0], impulse_response[0])
+        outr = scipy.signal.fftconvolve(signal[1], impulse_response[1])
+    else:
+        outl = scipy.signal.fftconvolve(signal[0], impulse_response)
+        outr = scipy.signal.fftconvolve(signal[1], impulse_response)
+    combo = np.vstack((outl, outr))
+    return combo / abs(combo).max()
+
+
+def main():
+    #generate_dry_mixtures()
+    #generate_reverb_mixtures()
+    #generate_duet_histograms('audio/mix/', 'Output/mix/')
+    generate_duet_histograms('audio/outputs/', 'Output/reverb/')
+
+
+if __name__ == '__main__':
+    main()
