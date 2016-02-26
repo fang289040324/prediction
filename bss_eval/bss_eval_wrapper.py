6f0c68254429ec81b52d714a4c9ccd23180b66a1
Ethan Manilow <eth@nmanilow.com>
2016-02-26 15:13:46 -0600

    Updating run_bss_eval. Adding bss_eval_wrapper.py

---
+++ bss_eval/bss_eval_wrapper.py
@@ -0,0 +1,24 @@
+import matlab_wrapper
+
+def bss_eval_sources_wrapper(estimated_signal, real_signal, cur_matlab_root=None):
+    """
+    Wrapper to call into bss_eval_sources in matlab. This will make it easier to call bss_eval from python.
+    :param estimated_signal: numpy array with dimensions [length, ch]
+    :param real_signal: numpy array with dimensions [length, ch]
+    :param cur_matlab_root: location of the matlab executable. To find this out, type matlabroot in matlab
+    and copy that path into here. If this function crashes without it, you probably need to add that string.
+    :return: list with [sdr, sar, sir, perm] values from bss_eval_sources
+    """
+    if cur_matlab_root is None:
+        matlab_session = matlab_wrapper.MatlabSession()
+    else:
+        matlab_session = matlab_wrapper.MatlabSession(matlab_root=cur_matlab_root)
+
+    matlab_session.put('estimated', estimated_signal)
+    matlab_session.put('real', real_signal)
+    matlab_session.eval('run_bss_eval')
+
+    bss_output = [matlab_session.get('sdr'), matlab_session.get('sar'), matlab_session.get('sir'),
+                      matlab_session.get('perm')]
+
+    return bss_output
--- bss_eval/run_bss_eval.m
+++ bss_eval/run_bss_eval.m
@@ -1,2 +1,1 @@
-[sdr_f, sir_f, sar_f, perm_f] = bss_eval_sources(fg_est, fg_real);
-[sdr_b, sir_b, sar_b, perm_b] = bss_eval_sources(bg_est, bg_real);
+[sdr, sir, sar, perm] = bss_eval_sources(estimated, real);
